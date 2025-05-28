67174110b2dd10c0b094f33443230829f7b1b8cc
NachoXGA <ignacio.x.gonzalez2@gmail.com>
2025-05-28 17:47:16 +0000

    proyecto carta

--- src/app.js
+++ src/app.js
@@ -6,12 +6,12 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 
-const palo = ["♥", "♦", "♠", "♣"];
-const number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const palos = ["♥", "♦", "♠", "♣"];
+const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 function getRandomCard() {
-    const palo = palo[Math.floor(Math.random() * palo.length)];
-    const number = number[Math.floor(Math.random() * palo.length)];
+    const palo = palos[Math.floor(Math.random() * palos.length)];
+    const number= numbers[Math.floor(Math.random() * numbers.length)];
     return { palo, number };
 }
 
