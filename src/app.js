ccb8887551284e70c511c56f82abd2ae0102092c
NachoXGA <ignacio.x.gonzalez2@gmail.com>
2025-05-28 17:22:57 +0000

    proyecto cartas

--- src/app.js
+++ src/app.js
@@ -6,19 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 
-const suits = ["♥", "♦", "♠", "♣"];
-const ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const palo = ["♥", "♦", "♠", "♣"];
+const number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 function getRandomCard() {
-    const suit = suits[Math.floor(Math.random() * suits.length)];
-    const rank = ranks[Math.floor(Math.random() * ranks.length)];
-    return { suit, rank };
+    const palo = palo[Math.floor(Math.random() * palo.length)];
+    const number = number[Math.floor(Math.random() * palo.length)];
+    return { palo, number };
 }
 
 function displayCard(card) {
-    document.getElementById("suit-top-left").textContent = card.suit;
-    document.getElementById("rank").textContent = card.rank;
-    document.getElementById("suit-bottom-right").textContent = card.suit;
+    document.getElementById("palo-top-left").textContent = card.palo;
+    document.getElementById("number").textContent = card.number;
+    document.getElementById("palo-bottom-right").textContent = card.palo;
 }
 
 document.getElementById("new-card-button").addEventListener("click", () => {
